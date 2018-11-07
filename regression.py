4f1dcff2136ae27a6041c3e3f3665fa69e637ccf
Salman Siddiqui <salmansiddiqui100@gmail.com>
2018-11-07 20:38:38 +0530

    Crap

--- regression.py
+++ regression.py
@@ -6,6 +6,7 @@ import quandl as q
 import math
 from sklearn import *
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 
 # Google Dataset for Test
@@ -18,7 +19,6 @@ df.fillna(-9999, inplace=True)
 forcast_out = int(math.ceil(0.1*(len(df))))
 df['label'] = df[forcast_column].shift(-forcast_out)
 df.dropna(inplace=True)
-print(df)
 
 x = np.array(df.drop(['label'],1))
 y = np.array(df['label'])
@@ -26,5 +26,8 @@ x = preprocessing.scale(x)
 df.dropna(inplace=True)
 y = np.array(df['label'])
 
-print(len(x))
-print(len(y))
+x_train, x_test, y_train, y_test = train_test_split(x, y , test_size=0.3)
+linear_regression = LinearRegression()
+linear_regression.fit(x_train, y_train)
+accuracy = linear_regression.score(x_test, y_test)
+print("Accuracy",  accuracy)
